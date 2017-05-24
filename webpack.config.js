e7d844a61d93b063f25549b294a11f02c02f3555
leo <cswleo@gmail.com>
2017-05-24 09:08:07 +0800

    css

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 const path = require('path')
 const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const config = require('./config')
 
 module.exports = {
-  entry: './src/main.js',
+  entry: {
+    app: ['./src/main.js'],
+    style: ['./src/css/application.sass']
+  },
   output: {
     path: config.assetBuildPath,
     publicPath: '',
-    filename: 'bundle.js'
+    filename: '[name].js?[hash:7]'
   },
   module: {
     rules: [
@@ -15,10 +19,32 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            'css-loader',
+            'postcss-loader',
+            'sass-loader'
+          ]
+        })
+      },
+      {
+        test: /img\/.*\.(png|jpg|jpeg|gif|svg)(\?[a-z0-9-]+)?$/,
+        loader: 'file-loader?name=[path][name]-[hash:7].[ext]'
+      },
+      {
+        test: /font\/.*\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9-]+)?$/,
+        loader: 'file-loader?name=[path][name]-[hash:7].[ext]'
       }
     ]
   },
   plugins: [
+     new webpack.DefinePlugin({
+        'process.env': config.env
+      }),
+      new ExtractTextPlugin({ filename: '[name].css?[hash:7]', allChunks: true }),
       // new webpack.optimize.UglifyJsPlugin()
   ]
 }
